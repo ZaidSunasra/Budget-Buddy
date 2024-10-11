e87b76b58f553d0303a14a64f947a3a1d1d93251
Zaid Arshad Sunasra <zaidsunasra26@gmail.com>
2024-10-11 12:01:39 +0530

    Chnaged type of id from number to string everywhere

--- server/src/services/expenseService.ts
+++ server/src/services/expenseService.ts
@@ -1,7 +1,7 @@
 import { db } from "../db/db"
 import { addTransaction, editTransaction } from "../types/types";
 
-export const addExpense = async ({title, type, transaction_time, amount}: addTransaction, id:number) : Promise<void> => {
+export const addExpense = async ({title, type, transaction_time, amount}: addTransaction, id:string) : Promise<void> => {
     await db.query("INSERT INTO expenses(title, type, transaction_time, amount, user_id) VALUES($1,$2, $3, $4, $5)", 
         [title, type, transaction_time, amount, id]);
 }
@@ -12,14 +12,14 @@ export const getExpense = async (id: string) : Promise<any> => {
 }
 
 export const getExpenseById = async (id: string) : Promise<any> => {
-    const query = await db.query("SELECT * FROM expenses WHERE transaction_id = $1", [id]);
+    const query = await db.query("SELECT * FROM expenses WHERE id = $1", [id]);
     return query.rows;
 }
 
 export const editExpense = async ({ title, type, amount} : editTransaction, id:string) : Promise<void> => {
-    await db.query("UPDATE expenses SET title=$1, type=$2, amount=$3 WHERE transaction_id=$4", [title, type, amount, id]);
+    await db.query("UPDATE expenses SET title=$1, type=$2, amount=$3 WHERE id=$4", [title, type, amount, id]);
 }
 
 export const deleteExpense = async (id:string) : Promise<void> => {
-    await db.query("DELETE FROM expenses WHERE transaction_id=$1", [id]);
+    await db.query("DELETE FROM expenses WHERE id=$1", [id]);
 }
