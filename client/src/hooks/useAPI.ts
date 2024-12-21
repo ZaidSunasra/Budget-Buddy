5bbfef2c1ae041002b4b210abfb01cea052f4f4c
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2024-12-21 20:59:04 +0530

    Datafetch hook

---
+++ client/src/hooks/useAPI.ts
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+export const getData = ({ url }: { url: string }) => {
+
+    const [apiData, setApiData] = useState<any>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [serverError, setServerError] = useState<any>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                const response = await axios.get(url);
+                setApiData(response.data);
+            } catch (error: any) {
+                setServerError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchData();
+    }, [url]);
+
+    return { apiData, isLoading, serverError };
+
+}
+
+export const postData = () => {
+
+    const [apiData, setApiData] = useState<any>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [serverError, setServerError] = useState<any>(null);
+
+    const fetchData = async ({ url, payload}: { url: string, payload: any}) => {
+        setIsLoading(true);
+        setServerError(null);
+        setApiData(null);
+        try {
+            console.log(payload)
+            const response = await axios.post(url, payload);
+            setApiData(response.data);
+        } catch (error: any) {
+            setServerError(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+    return {  fetchData, apiData, serverError, isLoading };
+
+}
