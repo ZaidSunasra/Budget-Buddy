8300933cf4fda70c5d304bdd869de0618aba095f
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2024-12-22 19:05:59 +0530

    Added routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing"
+import { Toaster } from "sonner";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Toaster position="bottom-right" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup" element = {<Signup />} />
+          <Route path="/dashboard" element = {<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
