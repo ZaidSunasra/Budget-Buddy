92de3b54f4e0d06e2e9b6075f306005a5405d3d1
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2024-12-22 19:06:41 +0530

    Added global types file

---
+++ client/src/types.ts
@@ -0,0 +1,11 @@
+export type loginInput = {
+    email: string,
+    password: string
+}
+
+export type signupInput = {
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string
+}
