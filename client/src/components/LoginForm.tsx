d1160afbada69614f2067dc8061dc7dcdb933cbc
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2024-12-23 20:43:03 +0530

    Added error checks to input

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -6,16 +6,17 @@ import { postData } from "@/hooks/useAPI";
 import { toast } from "sonner";
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { loginInput } from "@/types";
+import { baseURL, loginInput } from "@/types";
+import { ErrorMessage } from "./ErrorMessage";
 
 export function Login() {
-
+   
     const navigate = useNavigate();
-    const { handleSubmit, register } = useForm<loginInput>();
+    const { handleSubmit, register, formState: { errors, isSubmitting } } = useForm<loginInput>();
     const { fetchData, apiData, serverError, isLoading } = postData();
     const onSubmit: SubmitHandler<loginInput> = async (data) => {
         await fetchData({
-            url: "http://localhost:3000/api/v1/auth/signin",
+            url: `${baseURL}/auth/signin`,
             payload: {
                 email: data.email,
                 password: data.password
@@ -23,30 +24,40 @@ export function Login() {
         });
     }
 
-    useEffect (() => {
-        if(apiData){
+    useEffect(() => {
+        if (apiData) {
             toast(apiData.msg);
             navigate("/dashboard");
         }
-        if(serverError){
+        if (serverError) {
             toast(serverError.msg);
         }
     }, [apiData, serverError])
 
     return <div>
         <form className="p-8 border-primary border rounded-2xl bg-accent shadow-2xl" onSubmit={handleSubmit(onSubmit)}>
-            <h3 className="text-center text-2xl mb-4">Login to your account</h3>
+            <h3 className="text-center text-2xl mb-4 font-bold">Login to your account</h3>
             <div className="mb-4" >
                 <Label>Email</Label>
-                <Input {...register("email")} type="email" />
+                <Input {...register("email",
+                    {
+                        required: { value: true, message: "Email cannot be empty" },
+                    }
+                )} type="email" placeholder="m@example.com" />
+                {errors.email && <ErrorMessage message={errors.email.message} />}
             </div>
             <div className="mb-6" >
                 <Label>Password</Label>
-                <Input {...register("password")} type="password" />
+                <Input {...register("password",
+                    {
+                        required: { value: true, message: "Password cannot be empty." }
+                    }
+                )} type="password" />
+                {errors.password && <ErrorMessage message={errors.password.message} />}
             </div>
-            <Button type="submit" className="w-full mb-2"> {isLoading ? "Logging In" : "Login"} </Button>
+            <Button type="submit" className="w-full mb-2" disabled={isSubmitting}> {isLoading ? "Logging In" : "Login"} </Button>
             <p className="text-center">Dont have an account?<Link to={"/signup"} className="underline">SignUp</Link></p>
-        </form>    
+        </form>
     </div>
 
 }
