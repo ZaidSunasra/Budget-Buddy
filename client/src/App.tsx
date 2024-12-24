16940c31c661513e0014a7acc17f14edaed12b13
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2024-12-24 23:16:54 +0530

    Added routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,28 @@ import Landing from "./pages/Landing"
 import { Toaster } from "sonner";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import { SidebarProvider } from "./components/ui/sidebar";
+import Analytics from "./pages/Analytics";
+import Settings from "./pages/Settings";
+import Budget from "./pages/Budgeting";
+import { Loading } from "./components/Loading";
 function App() {
   return (
     <>
-      <Toaster position="bottom-right" />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/signup" element = {<Signup />} />
-          <Route path="/dashboard" element = {<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
+      <SidebarProvider>
+        <Toaster position="bottom-right" />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/budgeting" element={<Budget />} />
+            <Route path="/load" element={<Loading />} />
+          </Routes>
+        </BrowserRouter>
+      </SidebarProvider>
     </>
   )
 }
