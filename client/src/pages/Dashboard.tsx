0caec99e576b10f5b19ffd7abca6645f933c2f91
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2024-12-25 22:59:07 +0530

    Moved api to parent component

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,20 +1,45 @@
-import { ExpenseTable } from "@/components/ExpenseTable";
+import { TransactionTable } from "@/components/TransactionTable";
 import { SideBar } from "@/components/SideBar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Plus } from 'lucide-react';
+import { Loading } from "@/components/Loading";
+import { toast } from "sonner";
+import { getData } from "@/hooks/useAPI";
+import { baseURL } from "@/types";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
+
+    const navigate = useNavigate();
+
+    const { apiData, isLoading, serverError } = getData({
+        url: `${baseURL}/expense/get`
+    });
+
+    if (isLoading) {
+        return <>
+            <Loading />
+        </>
+    }
+
+    if (serverError) {
+        toast(serverError.msg)
+    }
+
     return <div className="flex w-screen">
         <SideBar />
         <SidebarTrigger />
         <div className="p-4 w-full">
             <div className="flex gap-5 mb-8">
                 <Input placeholder="Search expenses" className="w-4/5" type="search" />
-                <Button className="w-1/5 text-secondary flex gap-5"> <Plus /> Add Transaction </Button>
+                <Button className="w-1/5 text-secondary flex gap-5" onClick={() => navigate("/addTransaction")}> <Plus /> Add Transaction </Button>
             </div>
-            <ExpenseTable />
+            <div className="max-h-[85vh] overflow-y-auto border-2">
+            <TransactionTable data={apiData} />
+            </div>
+            
         </div>
     </div>
 }
