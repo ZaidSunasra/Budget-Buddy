cfd10b5836f7d638aa3426f96dd46d812bbab02c
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2024-12-26 20:56:48 +0530

    Added delete transaction feature

--- client/src/components/TransactionTable.tsx
+++ client/src/components/TransactionTable.tsx
@@ -1,12 +1,37 @@
-import { expenseInput } from "@/types";
+import { baseURL, expenseInput } from "@/types";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
 import { Button } from "./ui/button";
 import { Pencil, Trash2 } from 'lucide-react';
 import { useNavigate } from "react-router-dom";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogDescription, DialogTitle } from "./ui/dialog";
+import { postData } from "@/hooks/useAPI";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
-export function TransactionTable({ data }: { data: any }) {
 
-    const navigate = useNavigate(); 
+export function TransactionTable({ data, onDelete }: { data: any, onDelete: () => void}) {
+
+    const navigate = useNavigate();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const { fetchData, apiData, serverError, isLoading } = postData();
+    async function deleteTransaction(id: string) {
+        await fetchData({
+            url: `${baseURL}/expense/delete/${id}`,
+            payload: {},
+            method: "DELETE"
+        })
+    }
+    useEffect(() => {
+        if (apiData) {
+            toast(apiData.msg);
+            setIsOpen(false);
+            navigate("/dashboard");
+            onDelete();
+        } else if (serverError) {
+            toast(serverError.msg);
+        }
+    }, [apiData, serverError])
 
     return <div>
         <Table>
@@ -30,13 +55,29 @@ export function TransactionTable({ data }: { data: any }) {
                         <TableCell className={expense.type == "expense" ? "text-red-500" : "text-green-500"}>{expense.amount}</TableCell>
                         <TableCell className="flex gap-5">
                             <Button className="bg-secondary text-green-500" onClick={() => navigate(`/editTransaction/${expense.id}`)}> <Pencil /> </Button>
-                            <Button className="bg-secondary text-red-500"> <Trash2 /> </Button>
+                            <Button className="bg-secondary text-red-500" onClick={() => setIsOpen(true)}> <Trash2 /> </Button>
+                            <Dialog open={isOpen} onOpenChange={setIsOpen}>
+                                <DialogContent className="sm:max-w-md">
+                                    <DialogHeader>
+                                        <DialogTitle>Delete Transaction</DialogTitle>
+                                        <DialogDescription className="text-red-500">
+                                            Are you sure? This action cannot be undone.
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                    <DialogFooter className="sm:justify-start">
+                                        <Button type="submit" className="px-3 flex gap-1 items-center" variant="destructive" onClick={() => deleteTransaction(expense.id)}>
+                                            <span> {isLoading ? "Deleting" : "Delete"}</span>
+                                            <Trash2 />
+                                        </Button>
+                                    </DialogFooter>
+                                </DialogContent>
+                            </Dialog>
                         </TableCell>
                     </TableRow>
                 ))}
             </TableBody>
         </Table>
-    </div>
+    </div >
     
 }
 
