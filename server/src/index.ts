031bd5e936558ec9172fd3c1f4b803b6f8f4e883
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-01-07 23:53:18 +0530

    Changed url

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,11 @@ import env from 'dotenv';
 const app = express();
 env.config();
 
+app.options("*", cors({
+    origin: "https://budget-buddy-one-rose.vercel.app",  // Your frontend URL
+    credentials: true,
+}));
+
 app.use(cors({
     credentials: true,
     origin: "https://budget-buddy-one-rose.vercel.app/"
