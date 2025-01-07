f451c0cf239bcdfc116bb8a0503c85cd62dc6268
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-01-07 23:34:48 +0530

    Added headers

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,10 +12,17 @@ app.use(cors({
     origin: "https://budget-buddy-one-rose.vercel.app"
 }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://budget-buddy-one-rose.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1", mainRouter);
 
-app.listen(process.env.PORT, () =>{
+app.listen(process.env.PORT, () => {
     console.log(`Server running on Port:${process.env.PORT}`);
 })
