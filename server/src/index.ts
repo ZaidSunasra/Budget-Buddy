76c55cb7253a47c7ab32bc75085fef40fcf85cfd
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-01-07 21:31:18 +0530

    Deployed fe and be on cloud

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: "https://budget-buddy-one-rose.vercel.app/"
 }));
 
 env.config();
