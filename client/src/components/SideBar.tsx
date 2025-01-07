23098bb186d8f8251239f691a7355dc3ebc6cedb
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-01-07 18:13:53 +0530

    Changed background color of logo

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -88,7 +88,7 @@ export function SideBar() {
           <SidebarMenu>
             <SidebarMenuItem>
               <SidebarMenuButton className="font-bold text-2xl">
-                <div className="aspect-square size-6 rounded-lg flex items-center justify-center">
+                <div className="aspect-square size-6 rounded-lg flex items-center justify-center bg-sidebar">
                   <img src="../icons/logo.svg" alt="Logo" className="w-6 h-6 rounded" />
                 </div>
                 <span>Budget Buddy</span>
