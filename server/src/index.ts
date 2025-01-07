77eeb89390ebbf75978f44ce628aa4c213edb69e
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-01-07 23:49:46 +0530

    Changed url

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,15 +9,8 @@ env.config();
 
 app.use(cors({
     credentials: true,
-    origin: "https://budget-buddy-one-rose.vercel.app"
+    origin: "https://budget-buddy-one-rose.vercel.app/"
 }));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://budget-buddy-one-rose.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});
 
 app.use(express.json());
 app.use(cookieParser());
