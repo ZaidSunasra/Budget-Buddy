5680bd1cc2557d50983d77cb9bad9cfea0ecae43
Zaid Sunasra <110675217+ZaidSunasra@users.noreply.github.com>
2025-01-08 12:20:45 +0530

    Chnaded cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,13 +7,10 @@ import env from 'dotenv';
 const app = express();
 env.config();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://budget-buddy-one-rose.vercel.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    console.log("CORS headers set for:", req.method, req.url);
-    next();
-});
-
+app.use(cors({
+    origin: "https://budget-buddy-one-rose.vercel.app",
+    credentials: true
+}));
 
 
 app.use(express.json());
